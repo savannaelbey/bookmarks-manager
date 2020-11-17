11fc311e68f5b6abf9afbde04c7784ad88da009f
savannaelbey <savanna_e@icloud.com>
2020-11-17 15:53:44 +0000

    created Bookmark class and got the application and database talking to each other.

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -3,4 +3,10 @@ feature 'viewing bookmarks' do
     visit ('/')
     expect(page).to have_content "Bookmarks Manager"
   end
+  scenario 'seeing bookmarks' do
+    visit ('/bookmarks')
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+  end
 end

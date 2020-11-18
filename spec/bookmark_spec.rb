4fd2e42c1a5998166f9b29bb23c59dcd49a78776
savannaelbey <savanna_e@icloud.com>
2020-11-18 01:00:33 +0000

    created a test database (bookmark_manager_test) with an empty bookmarks table. Set up an Environmental Variable to tell the application to start in the test environment (and use the test database) when we want it to.Wrote a helper method(setup_test_database) that truncates (empties) the bookmarks table in the test database before each test run. in the feature test for listing bookmarks, I add some bookmarks to the test database and  wrapped the code in setup_test_database in a method of the same name, and called that method before each test (in spec_helper.rb file).

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,7 +2,11 @@ require 'bookmark'
 
 describe Bookmark do
   describe '.all' do
-    it 'returns all bookmarks' do
+    it 'returns alist of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
       bookmarks = Bookmark.all
       expect(bookmarks).to include("http://www.makersacademy.com")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,9 +1,17 @@
+
+require 'pg'
+
 feature 'viewing bookmarks' do
   scenario 'visiting the index page' do
     visit ('/')
     expect(page).to have_content "Bookmarks Manager"
   end
-  scenario 'seeing bookmarks' do
+  scenario 'seeing bookmarks list' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
     visit ('/bookmarks')
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.google.com"

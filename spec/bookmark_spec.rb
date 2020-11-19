64a0d6e7473b2df729549c7847a68da5b92dae8f
savannaelbey <savanna_e@icloud.com>
2020-11-19 01:12:09 +0000

    transforming the database interaction code from the controller to the model using tdd

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -13,4 +13,11 @@ describe Bookmark do
       expect(bookmarks).to include("http://www.google.com")
     end
   end
+
+  describe '.create()' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http:www.facebook.com')
+      expect(Bookmark.all).to include('http:www.facebook.com')
+    end
+  end
 end
